285b0675a73160b1fee932bbe42f37b7ecfe9f7a
Sarataxa <haimersara@gmail.com>
2023-03-16 06:00:18 -0700

    Heelo,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
-#include<studio.h>
+#include <studio.h>
 /**
- * main - A program that prints a line with puts function
- * Return : 0 (Success)
+ * main - Entry point
+ * the program prints a line with puts function
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
