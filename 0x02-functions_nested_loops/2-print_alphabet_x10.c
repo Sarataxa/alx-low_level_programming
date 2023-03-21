3f762e7bc7a69ec3441b819f01697ce6ad233417
Sarataxa <haimersara@gmail.com>
2023-03-21 12:33:38 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - print 10 times all alphabet in lowercase
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+int i;
+i = 0;
+while (i < 10)
+{
+print_alphabet();
+_putchar('\n');
+i++;
+}
