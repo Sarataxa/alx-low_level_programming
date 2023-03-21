e68163559b678ad6822a009aa2930162804563f2
Sarataxa <haimersara@gmail.com>
2023-03-21 11:49:39 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,5 +9,11 @@ int main(void)
 _putchar('_');
 _putchar('p');
 _putchar('u');
-return ();
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
