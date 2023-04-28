5ea5453a87a61a7bca651a69a2ff612ee093fcd5
Sarataxa <haimersara@gmail.com>
2023-04-27 17:04:59 -0700

    push all tasks

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,6 +3,23 @@
 #include "lists.h"
 
 /**
+ * _strlen - length of a given string
+ * @str: String
+ *
+ * Return: the length of the str
+ */
+int _strlen(char *str)
+{
+	int length = 0;
+
+	if (!str)
+		return (0);
+	while (*str++)
+		length++;
+	return (length);
+}
+
+/**
  * add_node - adds a new node at the beginning of a linked list
  * @head: double pointer to the list_t list
  * @str: new string to add in the node
